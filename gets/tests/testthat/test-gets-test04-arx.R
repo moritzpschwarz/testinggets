c9219c303ac60af44bc43a88e9812384080f9c0a
moritzpschwarz <moritz.schwarz@outlook.com>
2020-11-12 19:22:57 +0000

    Update test-gets-test04-arx.R

--- gets/tests/testthat/test-gets-test04-arx.R
+++ gets/tests/testthat/test-gets-test04-arx.R
@@ -497,8 +497,8 @@ SWtest <- function(x, ...){
 }
 
 # mod06 <- arx(y, ar=1:4, mxreg=mX, user.diagnostics=list(name="SWtest"))
-print(mod06)
-print(mod06, signif.stars=TRUE)
+#print(mod06)
+#print(mod06, signif.stars=TRUE)
 mod06 <- arx(y, ar=1:4, mxreg=mX,user.diagnostics=list(name="SWtest", pval=0.025))
 # the pval argument is ignored (as it should), I think
 print(mod06)
